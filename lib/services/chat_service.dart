0693eebf64cc0925a0ede928ab9e72bc190ddbfe
CCVpopular <datkhodo888@gmail.com>
2024-11-19 14:43:30 +0700

    add Stream

--- lib/services/chat_service.dart
+++ lib/services/chat_service.dart
@@ -1,36 +1,92 @@
+import 'dart:async';
+import 'dart:convert';
+import 'package:http/http.dart' as http;
 import 'package:socket_io_client/socket_io_client.dart' as IO;
 
 class ChatService {
-  IO.Socket? socket;
+  late IO.Socket socket;
+  final _messageStreamController = StreamController<Map<String, String>>.broadcast();
+  final String userId;
+  final String friendId;
 
-  // Khởi tạo kết nối socket
-  void initSocket() {
-    socket = IO.io('http://26.113.132.145:3000', IO.OptionBuilder().setTransports(['websocket']).build());
-    
-    socket!.onConnect((_) {
+  ChatService(this.userId, this.friendId) {
+    _connectSocket();
+  }
+
+  // Kết nối socket và lắng nghe sự kiện
+  void _connectSocket() {
+    socket = IO.io(
+      'http://26.113.132.145:3000',
+      IO.OptionBuilder().setTransports(['websocket']).build(),
+    );
+
+    socket.onConnect((_) {
       print('Connected to server');
+
+      // Tham gia phòng chat
+      socket.emit('joinRoom', {
+        'userId': userId,
+        'friendId': friendId,
+      });
     });
 
-    // Nhận toàn bộ tin nhắn khi kết nối
-    socket!.on('initMessages', (data) {
-      // Xử lý khi nhận tin nhắn ban đầu từ server
-    });
-
-    // Nhận tin nhắn mới
-    socket!.on('receiveMessage', (data) {
-      // Xử lý khi nhận tin nhắn mới từ server
+    // Lắng nghe sự kiện nhận tin nhắn
+    socket.on('receiveMessage', (data) {
+      if (data['sender'] != userId) {
+        _messageStreamController.add({
+          'sender': data['sender'],
+          'message': data['message'],
+        });
+      }
     });
   }
 
-  // Gửi tin nhắn
-  void sendMessage(String username, String message) {
-    socket!.emit('sendMessage', {
-      'username': username,
+  // Hàm gửi tin nhắn
+  void sendMessage(String message) {
+    socket.emit('sendMessage', {
+      'sender': userId,
+      'receiver': friendId,
+      'message': message,
+    });
+
+    // Thêm tin nhắn vào Stream ngay lập tức
+    _messageStreamController.add({
+      'sender': userId,
       'message': message,
     });
   }
 
+  // Hàm lấy tin nhắn cũ
+  Future<List<Map<String, String>>> loadMessages() async {
+    final url = Uri.parse('http://26.113.132.145:3000/api/messages/messages/$userId/$friendId');
+    try {
+      final response = await http.get(url);
+      if (response.statusCode == 200) {
+        final List<dynamic> data = jsonDecode(response.body);
+        return data.map((msg) {
+          return {
+            'sender': msg['sender'].toString(),
+            'message': msg['message'].toString(),
+          };
+        }).toList();
+      } else {
+        throw Exception('Failed to load messages: ${response.body}');
+      }
+    } catch (e) {
+      throw Exception('Error loading messages: $e');
+    }
+  }
+
+  // Stream để lắng nghe tin nhắn
+  Stream<Map<String, String>> get messageStream => _messageStreamController.stream;
+
+  // Đóng Stream và Socket
   void dispose() {
-    socket!.disconnect();
+    socket.emit('leaveRoom', {
+      'userId': userId,
+      'friendId': friendId,
+    });
+    socket.disconnect();
+    _messageStreamController.close();
   }
 }
