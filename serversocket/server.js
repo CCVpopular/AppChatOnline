73014354b96f8178be516fe32ab2233a176c9cc8
HienTran <hienxadoi2020@gmail.com>
2024-11-25 14:41:07 +0700

    sửa lỗi server.js

--- serversocket/server.js
+++ serversocket/server.js
@@ -29,8 +29,6 @@ app.set('socketio', io);
 app.use(express.json());
 
 // Routes
-
-
 app.use('/api/auth', authRoutes);
 app.use('/api/friends', friendRoutes);
 app.use('/api/messages', messageRoutes);
@@ -40,16 +38,14 @@ app.use('/api/rooms', roomRoutes);
 io.on('connection', (socket) => {
   console.log('New client connected');
 
+  // Người dùng tham gia phòng
   socket.on('joinRoom', ({ userId, friendId }) => {
     const roomName = [userId, friendId].sort().join('_');
-    console.log(`Current rooms for socket ${socket.id}:`, Array.from(socket.rooms));
     socket.join(roomName);
     console.log(`${userId} joined room ${roomName}`);
-
-    // const room = io.sockets.adapter.rooms.get(roomName);
-    // console.log(`Users in room ${roomName}:`, room ? room.size : 0);
   });
 
+  // Gửi tin nhắn cá nhân
   socket.on('sendMessage', async (data) => {
     try {
       const { sender, receiver, message } = data;
@@ -58,140 +54,57 @@ io.on('connection', (socket) => {
         console.error('Invalid message data:', data);
         return;
       }
-  
 
       const roomName = [sender, receiver].sort().join('_');
-
       const newMessage = new Message({ sender, receiver, message });
       await newMessage.save();
-  
 
       io.to(roomName).emit('receiveMessage', data);
     } catch (err) {
       console.error('Error handling sendMessage:', err);
     }
-  });  
+  });
 
+  // Người dùng rời phòng
   socket.on('leaveRoom', ({ userId, friendId }) => {
     const roomName = [userId, friendId].sort().join('_');
     socket.leave(roomName);
     console.log(`${userId} left room ${roomName}`);
   });
 
-
-  // Join a group
+  // Tham gia nhóm
   socket.on('joinGroup', ({ groupId, userId }) => {
     socket.join(groupId);
     console.log(`${userId} joined group: ${groupId}`);
     socket.to(groupId).emit('userJoined', `${userId} has joined the group.`);
   });
 
-  // Send a message to a group
-  socket.on('sendGroupMessage', async ({ groupId, sender, message }) => {
-    try {
-      // Emit the message to the group
-      io.to(groupId).emit('receiveGroupMessage', { sender, message });
-    } catch (err) {
-      console.error('Error sending group message:', err);
-    }
-  });
-  
-
-  socket.on('disconnect', () => {
-    console.log('Client disconnected');
+  // Gửi tin nhắn nhóm
+  socket.on('sendGroupMessage', ({ groupId, sender, message }) => {
+    io.to(groupId).emit('receiveGroupMessage', { sender, message });
   });
 
-  
-});
-
-
-
-  socket.on('joinRoom', ({ userId, friendId }) => {
-    const roomName = [userId, friendId].sort().join('_');
-    socket.join(roomName);
-    console.log(`${userId} joined room ${roomName}`);
-  });
-
-
-  socket.on('offer', (data) => {
-    const { roomName, sdp } = data;
+  // Signaling cho WebRTC
+  socket.on('offer', ({ roomName, sdp }) => {
     console.log(`Offer received for room ${roomName}`);
     socket.to(roomName).emit('offer', { sdp, from: socket.id });
   });
 
-  socket.on('answer', (data) => {
-    const { roomName, sdp } = data;
+  socket.on('answer', ({ roomName, sdp }) => {
     console.log(`Answer received for room ${roomName}`);
     socket.to(roomName).emit('answer', { sdp, from: socket.id });
   });
 
-  socket.on('candidate', (data) => {
-    const { roomName, candidate } = data;
+  socket.on('candidate', ({ roomName, candidate }) => {
     console.log(`ICE candidate received for room ${roomName}`);
     socket.to(roomName).emit('candidate', { candidate, from: socket.id });
   });
 
-  socket.on('leaveRoom', ({ userId, friendId }) => {
-    const roomName = [userId, friendId].sort().join('_');
-    socket.leave(roomName);
-    console.log(`${userId} left room ${roomName}`);
-  });
-
+  // Ngắt kết nối
   socket.on('disconnect', () => {
     console.log('Client disconnected');
   });
-// });
-
-// io.on('connection', (socket) => {
-//   console.log('New client connected');
-
-//   // Xử lý khi người dùng tham gia phòng
-//   socket.on('joinRoom', ({ userId, friendId }) => {
-//     const roomName = [userId, friendId].sort().join('_');
-//     socket.join(roomName);
-//     console.log(`${userId} joined room ${roomName}`);
-//   });
-
-//   // Signaling: Xử lý 'offer' từ peer A
-//   socket.on('offer', (data) => {
-//     const { roomName, sdp } = data;
-//     console.log(`Offer received for room ${roomName}`);
-//     // Gửi SDP của offer tới các peer khác trong phòng
-//     socket.to(roomName).emit('offer', { sdp, from: socket.id });
-//   });
-
-//   // Signaling: Xử lý 'answer' từ peer B
-//   socket.on('answer', (data) => {
-//     const { roomName, sdp } = data;
-//     console.log(`Answer received for room ${roomName}`);
-//     // Gửi SDP của answer tới peer đã gửi offer
-//     socket.to(roomName).emit('answer', { sdp, from: socket.id });
-//   });
-
-//   // Signaling: Xử lý ICE Candidate
-//   socket.on('candidate', (data) => {
-//     const { roomName, candidate } = data;
-//     console.log(`ICE candidate received for room ${roomName}`);
-//     // Gửi ICE Candidate tới các peer khác trong phòng
-//     socket.to(roomName).emit('candidate', { candidate, from: socket.id });
-//   });
-
-//   // Người dùng rời phòng
-//   socket.on('leaveRoom', ({ userId, friendId }) => {
-//     const roomName = [userId, friendId].sort().join('_');
-//     socket.leave(roomName);
-//     console.log(`${userId} left room ${roomName}`);
-//   });
-
-//   // Ngắt kết nối
-//   socket.on('disconnect', () => {
-//     console.log('Client disconnected');
-//   });
-// });
-
-
-
-
+});
 
 // Start server
 server.listen(3000, '0.0.0.0', () => console.log('Server is running on :3000'));
