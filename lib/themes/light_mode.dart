b31a5b2d5523141e51113c907de1fafc24fe9fa2
Phune23 <phutranbs23@gmail.com>
2024-11-25 20:57:46 +0700

    change color surface

--- lib/themes/light_mode.dart
+++ lib/themes/light_mode.dart
@@ -2,7 +2,7 @@ import 'package:flutter/material.dart';
 
 ThemeData lightMode = ThemeData(
   colorScheme: const ColorScheme.light(
-    surface: Color.fromARGB(255, 168, 228, 231), // Màu nền
+    surface: Color.fromARGB(255, 251, 251, 251), // Màu nền
     primary: Color.fromARGB(255, 5, 66, 66), //màu nút và nàu chữ trong nút
     secondary: Color.fromARGB(255, 184, 110, 41),
     tertiary: Color.fromARGB(255, 228, 129, 37),
