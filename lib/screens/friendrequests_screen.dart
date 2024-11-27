b5f8ba987683570434731f6de96e90f7d2d954b4
CCVpopular <datkhodo888@gmail.com>
2024-11-27 23:02:57 +0700

    Revert "ok"
    This reverts commit ea385253584e77ea906fdb67b848250b455987ca.

--- lib/screens/friendrequests_screen.dart
+++ lib/screens/friendrequests_screen.dart
@@ -1,5 +1,4 @@
 import 'dart:convert';
-import 'package:appchatonline/config/config.dart';
 import 'package:flutter/material.dart';
 import 'package:http/http.dart' as http;
 
@@ -15,7 +14,6 @@ class FriendRequestsScreen extends StatefulWidget {
 class _FriendRequestsScreenState extends State<FriendRequestsScreen> {
   List<Map<String, dynamic>> friendRequests = [];
   bool isLoading = true;
-  final String baseUrl = Config.apiBaseUrl;
 
   @override
   void initState() {
@@ -25,7 +23,7 @@ class _FriendRequestsScreenState extends State<FriendRequestsScreen> {
 
   Future<void> _loadFriendRequests() async {
     try {
-      final url = Uri.parse('http://$baseUrl/api/friends/friend-requests/${widget.userId}');
+      final url = Uri.parse('http://26.113.132.145:3000/api/friends/friend-requests/${widget.userId}');
       final response = await http.get(url);
       if (response.statusCode == 200) {
         final List<dynamic> data = jsonDecode(response.body);
@@ -52,7 +50,7 @@ class _FriendRequestsScreenState extends State<FriendRequestsScreen> {
 
   Future<void> _acceptRequest(String requestId) async {
     try {
-      final url = Uri.parse('http://$baseUrl/api/friends/accept-friend');
+      final url = Uri.parse('http://26.113.132.145:3000/api/friends/accept-friend');
       final response = await http.post(
         url,
         headers: {'Content-Type': 'application/json'},
