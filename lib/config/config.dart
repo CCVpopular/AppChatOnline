417422422e427687c5d453dd56b5e421fbfe8aa2
CCVpopular <datkhodo888@gmail.com>
2024-12-05 17:51:58 +0700

    update config files: remove Gemini API key and rename config.dart to configapi.dart

--- lib/config/config.dart
+++ lib/config/config.dart
@@ -1,4 +1,3 @@
 class Config {
   static const String apiBaseUrl = 'http://26.113.132.145:3000';
-  static const String GeminiAPI = 'https://generativelanguage.googleapis.com/v1beta/models/gemini-1.5-flash-latest:generateContent?key=AIzaSyA6aaYs2Z02DVx_4FSPRH4mL21erBWPMpU';
 }
